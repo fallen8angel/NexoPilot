3d14d3eee15c512cffd066a763740ba269d10bca
fallen8angel <75917889+fallen8angel@users.noreply.github.com>
2026-08-24 19:58:06 +0900

    Fix boot loop from stale ReverseDriverCamera param build

--- selfdrive/ui/mici/layouts/settings/toggles.py
+++ selfdrive/ui/mici/layouts/settings/toggles.py
@@ -18,7 +18,6 @@ class TogglesLayoutMici(NavScroller):
     is_metric_toggle = BigParamControl("use metric units", "IsMetric")
     ldw_toggle = BigParamControl("lane departure warnings", "IsLdwEnabled")
     always_on_dm_toggle = BigParamControl("always-on driver monitor", "AlwaysOnDM")
-    reverse_driver_camera = BigParamControl("show driver camera in reverse", "ReverseDriverCamera")
     record_front = BigParamControl("record & upload driver camera", "RecordFront", toggle_callback=restart_needed_callback)
     record_mic = BigParamControl("record & upload mic audio", "RecordAudio", toggle_callback=restart_needed_callback)
     enable_openpilot = BigParamControl("enable openpilot", "OpenpilotEnabledToggle", toggle_callback=restart_needed_callback)
@@ -29,7 +28,6 @@ class TogglesLayoutMici(NavScroller):
       is_metric_toggle,
       ldw_toggle,
       always_on_dm_toggle,
-      reverse_driver_camera,
       record_front,
       record_mic,
       enable_openpilot,
@@ -41,7 +39,6 @@ class TogglesLayoutMici(NavScroller):
       ("IsMetric", is_metric_toggle),
       ("IsLdwEnabled", ldw_toggle),
       ("AlwaysOnDM", always_on_dm_toggle),
-      ("ReverseDriverCamera", reverse_driver_camera),
       ("RecordFront", record_front),
       ("RecordAudio", record_mic),
       ("OpenpilotEnabledToggle", enable_openpilot),
